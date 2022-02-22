45216a01306028b543e11b9ab104df64cc744816
Ansh Lalwani <57198075+TheDarkFyre@users.noreply.github.com>
2022-02-21 22:24:35 -0800

    Add files via upload

---
+++ audiorecorder.py
@@ -0,0 +1,24 @@
+#from scipy.io import wavfile
+
+#def freq(file, start_time, end_time):
+#    sample_rate, data = wavfile.read(file)
+#    start_point = int(sample_rate * start_time / 1000)
+#    end_point = int(sample_rate * end_time / 1000)
+#    length = (end_time - start_time) / 1000
+#    counter = 0
+#   for i in range(start_point, end_point):
+#        if data[i] < 0 and data[i+1] > 0:
+#            counter += 1
+#    return counter/length    
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file 
+
+#freq("output.wav", 1000 ,2100)
